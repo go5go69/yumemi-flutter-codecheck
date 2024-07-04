de04061245e5489bfd9fbbaf88da436c5c2ccd08
go <go5go69@gmail.com>
2024-07-04 18:56:05 +0900

    ✨ flavorizrによる環境分け(dev, prd)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:yumemi_flutter_codecheck/main.dart';
+import 'package:yumemi_flutter_codecheck/flavors.dart';
+import 'package:yumemi_flutter_codecheck/pages/my_home_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  // 仮のテストです
+  testWidgets('MyHomePage has a title and message',
+      (WidgetTester tester) async {
+    // テスト用のウィジェットツリーを作成
+    await tester.pumpWidget(MaterialApp(home: MyHomePage()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // AppBarのタイトルを探す
+    expect(find.text(F.title), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Bodyのメッセージを探す
+    expect(find.text('Hello ${F.title}'), findsOneWidget);
   });
 }
